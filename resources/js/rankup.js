3666e67a76cd04442037fde9c203fb91f6fcdcd7
GGLap <ggonza55@uic.edu>
2024-01-18 02:53:30 -0600

    Removed redundant mainRow div & deleting row now moves contents to imgContainer at bottom.

--- resources/js/rankup.js
+++ resources/js/rankup.js
@@ -5,7 +5,6 @@ class RankRow {
         this.element.className = "RankRow Container";
         this.element.ondrop = (event) => drop(event);
         this.element.ondragover = (event) => allowDrop(event);
-        // this.element.ondragend = (event) => dragEnd(event);
 
         // Add this new row to the rowContainer
         const rowContainer = document.getElementById("rowList");
@@ -15,10 +14,17 @@ class RankRow {
 
 class rowFull {
     constructor(id) {
-        // DONE: Rework this class to be a row made up of a rowHeader, rowBody, and a rowTab. MainRow contains header and body, and rowTab is outside of MainRow inside rowFull.
+        // RowFull is the main container for the row. It contains the rowHeader and the rowBody.
         let rowFull = document.createElement("div");
         rowFull.className = "rowFull";
         rowFull.id = "rowFull" + id;
+        // DONE: rowHeader will be the element that contains the row's title and the row's delete and reset buttons.
+        // rowHeader consists of the title on the left, and the delete and reset buttons on the right side corners. The reset button is in the top right corner, and the delete button is in the bottom right corner. This can be done with a flexbox and an empty between the two buttons vertically.
+        let rowHeader = document.createElement("div");
+        rowHeader.className = "rowHeader rowPiece";
+        rowHeader.id = "rowHeader" + id;
+        rowHeader.onmouseover = () => showRowTab(rowHeader);
+        rowHeader.onmouseout = () => hideRowTab(rowHeader);
         // DONE: rowTab will be the left-most element that will be used to drag the row up and down, as well as have buttons to add a new row above or below it.
         // rowTab consists of an addRowAbove button, an addRowBelow button, and a drag handle in between, stacked vertically.
         let rowTab = document.createElement("div");
@@ -38,17 +44,6 @@ class rowFull {
         rowTab.appendChild(addRowAboveButton);
         rowTab.appendChild(dragHandle);
         rowTab.appendChild(addRowBelowButton);
-        // DONE: mainRow will be the element that contains the rowHeader and rowBody.
-        let MainRow = document.createElement("div");
-        MainRow.className = "mainRow";
-        MainRow.id = "rowFull" + id;
-        // DONE: rowHeader will be the element that contains the row's title and the row's delete and reset buttons.
-        // rowHeader consists of the title on the left, and the delete and reset buttons on the right side corners. The reset button is in the top right corner, and the delete button is in the bottom right corner. This can be done with a flexbox and an empty between the two buttons vertically.
-        let rowHeader = document.createElement("div");
-        rowHeader.className = "rowHeader rowPiece";
-        rowHeader.id = "rowHeader" + id;
-        rowHeader.onmouseover = () => showRowTab(rowHeader);
-        rowHeader.onmouseout = () => hideRowTab(rowHeader);
         // Add the title to the rowHeader
         let rowTitle = document.createElement("div");
         rowTitle.className = "rowTitle";
@@ -75,7 +70,7 @@ class rowFull {
         let deleteButtonImage = document.createElement("img");
         deleteButtonImage.src = "/resources/images/RowHeaderDelete.png";
         deleteButtonImage.className = "deleteButtonImage resetDeleteImage";
-        deleteButtonImage.onclick = () => rowFull.remove(); //TESTME: Does this also delete any images added to the row? If so, make sure they're moved back to the original container before deleting the row.
+        deleteButtonImage.onclick = () => deleteRow(rowFull);
         deleteButton.appendChild(deleteButtonImage);
         resetDeleteContainer.appendChild(resetButton);
         resetDeleteContainer.appendChild(emptyDiv);
@@ -84,8 +79,8 @@ class rowFull {
         rowHeader.appendChild(rowTab);
         rowHeader.appendChild(rowTitle);
         rowHeader.appendChild(resetDeleteContainer);
-        // Add the rowHeader to the mainRow
-        MainRow.appendChild(rowHeader);
+        // Add the rowHeader to the full row
+        rowFull.appendChild(rowHeader);
         // DONE: rowBody will be the element that contains the row's ranking images.
         // rowBody is the container the user will drag the images into to rank them.
         let rowBody = document.createElement("div");
@@ -93,12 +88,9 @@ class rowFull {
         rowBody.id = "rowBody" + id;
         rowBody.ondrop = (event) => drop(event);
         rowBody.ondragover = (event) => allowDrop(event);
-        // rowBody.ondragend = (event) => dragEnd(event);
         // Add the rowBody to the mainRow
-        MainRow.appendChild(rowBody);
-
-        // Add the mainRow to the rowFull
-        rowFull.appendChild(MainRow);
+        rowFull.appendChild(rowBody);
+        // Add the full row to the rowContainer
         let rowContainer = document.getElementById("rowList");
         rowContainer.appendChild(rowFull);
     }
@@ -127,17 +119,31 @@ function drop(ev) {
     }
 }
 
-// function dragEnd(ev) {
-//     ev.target.style.visibility = "visible";
-// }
-
 function showRowTab(element) {
-    rowTab = element.parentElement.parentElement.getElementsByClassName("rowTab")[0];
+    rowTab = element.parentElement.getElementsByClassName("rowTab")[0];
     rowTab.style.visibility = "visible";
 
 }
 
 function hideRowTab(element) {
-    rowTab = element.parentElement.parentElement.getElementsByClassName("rowTab")[0];
+    rowTab = element.parentElement.getElementsByClassName("rowTab")[0];
     rowTab.style.visibility = "hidden";
+}
+
+// DeleteRow - Deletes a specified row and moves any contents to the imageContainer at the bottom of the page.
+function deleteRow(row) {
+    // First check if this row had any images in it, if so, move them to the imageContainer
+    let rowBody = row.getElementsByClassName("rowBody")[0];
+    if (rowBody.hasChildNodes()) {
+        let imageContainer = document.getElementById("imageContainer");
+        while (rowBody.hasChildNodes()) {
+            imageContainer.appendChild(rowBody.firstChild);
+        }
+    }
+    row.remove();
+}
+
+// AddRow - Adds a new row above or below the specified row.
+function addRow(row, above = true) {
+    // TODO: Implement adding a row above or below the specified row.
 }
