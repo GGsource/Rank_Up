5c713bcb87733edde329f29be9b43a7458b86b53
GGMachina <git@ggsource.dev>
2026-02-15 17:51:17 -0600

    Fixing relative path to rankup.html

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ addEventListener("DOMContentLoaded", () => {
 	const newRankUpBtn = document.getElementById("create-new-rankup-card");
 	if (newRankUpBtn) {
 		newRankUpBtn.addEventListener("click", (event) => {
-			window.location.href = "/src/pages/rankup/rankup.html";
+			window.location.href = "src/pages/rankup/rankup.html";
 		});
 	}
 });
