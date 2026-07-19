4056339bf960b58206424ae9f1b05201436d088c
GGsource <GGsource@users.noreply.github.com>
2026-07-19 06:05:10 +0000

    deploy: d4ca8ccdc0498bf7b1e4698a994cb2459191e4f2

--- assets/home-S-EI6dXI.js
+++ assets/home-S-EI6dXI.js
@@ -1,10 +1,10 @@
-import{t as e}from"./index-BPm2-TPR.js";var t=`<!-- This is the main page we first arrive at. Since guest experience is priority, this page should work whether or not you are logged in -->
+import{t as e}from"./index-Csmq466-.js";var t=`<!-- This is the main page we first arrive at. Since guest experience is priority, this page should work whether or not you are logged in -->
 <!-- Therefore the layout should be: -->
 <!-- Just a basic centered horizontal list of cards displaying the 3 most recently created lists by this guest/user. -->
 <!-- On the right of these is a card with a "+" Icon, which can be selected to start creating a new rankup. -->
 <!-- Hovering or keying between any of these should leave a blue border around the selection and populate some centered text below showing either the title of the rankup or 'create new' -->
 <!-- If less than 3 recent rankups exist for this guest/user, then only those will be shown and then the '+' card. If none, then just the plus card -->
-<section class="home-view">
+<section class="page-view home-view">
 	<div id="cardbox">
 		<button id="placeholder-1" class="placeholder-card home-cards" type="button">Placeholder 1</button>
 		<button id="placeholder-2" class="placeholder-card home-cards" type="button">Placeholder 2</button>
