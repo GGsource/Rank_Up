d4ca8ccdc0498bf7b1e4698a994cb2459191e4f2
GG <60764091+GGsource@users.noreply.github.com>
2026-07-19 01:04:53 -0500

    Add Dev Environment Mode Banner (#42)
    * Moved banner CSS to global file
    
    * Revamped CSS architecture to be consistent across views and function with new components that are consistent across views. Removed some console logs. Contributes to #36
    
    * Returned image res to standard value and moved some new TODOs into new issues
    
    * Improved banner appearance and added function to populate or disable banner. Closes #36

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { renderPage } from "./components/renderPage";
 import "@/styles/global.css";
 
-renderPage("home");
+document.addEventListener("DOMContentLoaded", () => {
+	/* ----------------------------- Set Mode Banner ---------------------------- */
+	const modeBanner = document.getElementById("mode-banner");
+	if (!modeBanner) throw new Error("Couldn't find the mode banner to set :/");
+	if (window.location.hostname.startsWith("rankup.ggsource")) modeBanner.remove();
+	else modeBanner.innerText = import.meta.env.DEV ? "Dev Mode" : "Build Preview";
+
+	/* --------------------------- Render the homepage -------------------------- */
+	renderPage("home");
+});
