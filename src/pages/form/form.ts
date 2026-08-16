30e40a33ad6fa61dc36b8c97f3e4ae2a1896fb91
GGMachina <git@ggsource.dev>
2026-08-16 15:25:12 -0500

    Applied palette for form page, changed delete buttons to not show up on tab focus.

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -95,9 +95,8 @@ class FormPage extends Page {
 			imageWrapper.append(newImage);
 
 			// Make the delete button
-			const deleteButton = document.createElement("button") as HTMLButtonElement;
+			const deleteButton = document.createElement("div") as HTMLDivElement;
 			deleteButton.className = "delete-button";
-			deleteButton.type = "button";
 			deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>`;
 			deleteButton.addEventListener("click", (event) => {
 				event.stopPropagation();
