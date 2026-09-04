bcb2f27eec31b5713b0eccb3f4fc3ec3d73abfdd
GGMachina <git@ggsource.dev>
2026-09-03 22:43:15 -0500

    Added Function header for getEl utility

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Gets the element specified and does check to make sure its valid. Casts to specific element type if provided.
+ *
+ * @param elementName the ID of the element to fetch
+ * @returns the element specified
+ */
 export function getEl<ElementType extends HTMLElement = HTMLElement>(elementName: string): ElementType {
 	const element = document.getElementById(elementName) as ElementType;
 	if (!element) throw new Error(`Fatal Error: Failed to locate #${elementName}`);
